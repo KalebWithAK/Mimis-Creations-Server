4d360f3b8fc10f8ff063c3b4fe5241d0a15e07f1
KalebWithAK <kad9603@gmail.com>
2021-09-05 12:43:41 -0400

    transactions array instead of 1 object

--- models/order.js
+++ models/order.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    _id: Schema.Types.ObjectId,
+    _id: mongoose.Schema.Types.ObjectId,
     customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
     products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', quantity: { type: Number, default: 1 }}],
     status: { type: String, enum: ['PROCESSING', 'CONFIRMED', 'SHIPPING', 'DELIVERED'], default: 'PROCESSING', required: true },
     completed: { type: Boolean, default: false },
-    transaction: { 
-        confirmation: { type: String, required: true },
-        customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer'},
-        paymentMethod: { type: String, required: true }, // square, venmo, paypal
-        amount: { type: Number, required: true },
-        date: { type: Date, default: Date.now() }
-    }
+    transactions: [{ 
+        amount: {
+            total: { type: Number, required: true },
+            currency: { type: String, default: 'USD' },
+            details: {
+                subtotal: { type: Number, required: true },
+                tax: { type: Number, required: true }
+            }
+        }
+    }]
 }, { timestamp: true });
 
 const Order = mongoose.model('Order', orderSchema);
